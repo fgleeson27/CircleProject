6c1cc2af21b5d6a9f52243b7a2a31a7e11d1ad64
Frank G <s1226135@monmouth.edu>
2020-04-17 19:19:24 -0400

    Circle

---
+++ src/Circle.java
@@ -0,0 +1,45 @@
+public class Circle
+{
+
+	private double radius;
+
+	public Circle()
+	{
+	}
+
+	public Circle(double radius)
+	{
+		this.radius = radius;
+	}
+
+	//Return radius of the circle
+	public double getRadius()
+	{
+		return radius;
+	}
+
+	//Set a new radius for the circle
+	public void setRadius(double radius)
+	{
+		this.radius = radius;
+	}
+
+	//Write a method below this line called getArea to return the area of the circle
+	public double getArea () {
+		double area = (Math.pow(radius, 2) * Math.PI);
+		return area;
+	}
+
+	//Write a method below this line called getDiameter to return the diameter of the circle
+	public double getDiameter () {
+		double diameter = radius * 2;
+		return diameter;
+	}
+
+	//Write a method below this line called getPerimeter to return the perimeter of the circle
+	double getPerimeter () {
+		double perimeter = (radius*2) * Math.PI;
+		return perimeter;
+	}
+
+}
